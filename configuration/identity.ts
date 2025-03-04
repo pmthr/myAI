c4de152940db20782644267261b8ed03199af770
pooja mathur <pmathur@unc.edu>
2025-03-04 17:06:11 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Pooja Mathur`;
+export const OWNER_DESCRIPTION: string = `COMP 488 S25 Student`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Jurisight`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
