c0356d8f59ee51dc0df0a13a1f7b4de0ed2cbc75
pmthr <pmathur@unc.edu>
2025-03-06 20:15:13 -0500

    revert

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,35 @@
-import type { Metadata } from "next"
-import { ReactNode } from "react"
-import localFont from "next/font/local"
-import "./globals.css"
-import { ErrorWrapper } from "./parts/error/error-wrapper"
-import { TooltipProvider } from "@/components/ui/tooltip"
-import { PAGE_TITLE, PAGE_DESCRIPTION } from "@/configuration/ui"
-import ClientThemeProvider from "./theme-provider"
+"use client";
 
-export const metadata: Metadata = {
-  title: PAGE_TITLE,
-  description: PAGE_DESCRIPTION,
-}
+import ChatInput from "@/components/chat/input";
+import ChatMessages from "@/components/chat/messages";
+import useApp from "@/hooks/use-app";
+import ChatHeader from "@/components/chat/header";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-})
+export default function Chat() {
+  const {
+    messages,
+    handleInputChange,
+    handleSubmit,
+    input,
+    isLoading,
+    indicatorState,
+    clearMessages,
+  } = useApp();
 
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-})
-
-export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <TooltipProvider>
-          <ErrorWrapper>
-            <ClientThemeProvider>{children}</ClientThemeProvider>
-          </ErrorWrapper>
-        </TooltipProvider>
-      </body>
-    </html>
-  )
+    <>
+      <ChatHeader clearMessages={clearMessages} />
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-col max-w-screen-lg w-full h-full p-5">
+          <ChatMessages messages={messages} indicatorState={indicatorState} />
+        </div>
+      </div>
+      <ChatInput
+        handleInputChange={handleInputChange}
+        handleSubmit={handleSubmit}
+        input={input}
+        isLoading={isLoading}
+      />
+    </>
+  );
 }
